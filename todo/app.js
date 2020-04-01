394cf204aad20dd7ca7277db78e81710d8d67d98
Chris Norris <chrisnorris070@gmail.com>
2020-04-01 09:26:07 -0500

    Initial commit

---
+++ todo/app.js
@@ -0,0 +1,13 @@
+var _a;
+var array = [
+    "Pick up drycleaning",
+    "Clean up Batcave",
+    "Save Gotham"
+];
+for (var _i = 0, array_1 = array; _i < array_1.length; _i++) {
+    var value = array_1[_i];
+    console.log("" + value);
+}
+var a = 1;
+var b = 2;
+_a = [b, a], a = _a[0], b = _a[1];
